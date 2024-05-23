63a9b32f0b74e55ae5864b9e366ab22447bc3a5d
Obua Denis Daniel <dd.obua>
2024-05-23 13:31:26 +0300

    Include starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Obua</p>
+    </div>
+  );
 }
 
 export default App;
--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -3,8 +3,9 @@ const API_URL = 'https://react-fast-pizza-api.onrender.com/api';
 export async function getMenu() {
   const res = await fetch(`${API_URL}/menu`);
 
-  // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
-  if (!res.ok) throw Error('Failed getting menu');
+  // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually.
+  // This will then go into the catch block, where the message is set
+  if (!res.ok) throw Error('Failed to get menu');
 
   const { data } = await res.json();
   return data;
@@ -17,7 +18,7 @@ export async function getOrder(id) {
   const { data } = await res.json();
   return data;
 }
-+
+
 export async function createOrder(newOrder) {
   try {
     const res = await fetch(`${API_URL}/order`, {
@@ -32,7 +33,7 @@ export async function createOrder(newOrder) {
     const { data } = await res.json();
     return data;
   } catch {
-    throw Error('Failed creating your order');
+    throw Error('Failed to create your order');
   }
 }
 
@@ -49,6 +50,6 @@ export async function updateOrder(id, updateObj) {
     if (!res.ok) throw Error();
     // We don't need the data, so we don't return anything
   } catch (err) {
-    throw Error('Failed updating your order');
+    throw Error('Failed to update your order');
   }
 }
