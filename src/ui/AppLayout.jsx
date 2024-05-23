6c3d4c1ad3ebf9fb54a514037eaae6dccb0abf06
Obua Denis Daniel <dd.obua>
2024-05-23 19:51:06 +0300

    Remove public folder

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,13 +6,12 @@ import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
-  // console.log(navigation);
   const isLoading = navigation.state === 'loading';
 
   return (
     <div className="layout">
       {isLoading && <Loader />}
-      
+
       <Header />
 
       <Main>
