5412d9ea0f3db2c19448c7f3301f95a225574167
Obua Denis Daniel <dd.obua>
2024-05-24 12:42:40 +0300

    Include error element in route

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,12 @@ import CreateOrder from './features/order/CreateOrder';
 import OrderItem from './features/order/OrderItem';
 import AppLayout from './ui/AppLayout';
 
+import Error from './ui/Error';
+
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/menu', element: <Menu />, loader: menuLoader },
