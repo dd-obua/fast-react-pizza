3eb93bfdcd7ce7581c0e844f69fe2b0651eb5d33
Obua Denis Daniel <dd.obua>
2024-05-24 13:06:48 +0300

    Place data fetching error within the application layout

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,12 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
-      { path: '/menu', element: <Menu />, loader: menuLoader },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: '/cart', element: <Cart /> },
       { path: '/order', element: <Order /> },
       { path: '/order/new', element: <CreateOrder /> },
