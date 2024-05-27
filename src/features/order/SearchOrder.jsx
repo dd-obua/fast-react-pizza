ae96ff196efe1377dd6ba949886c5d2b4a213af3
Obua Denis Daniel <dd.obua>
2024-05-27 18:58:42 +0300

    Style search bar

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -10,7 +10,7 @@ function SearchOrder() {
     if (!query) return;
     navigate(`/order/${query}`);
     setQuery('');
-  } 
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -18,7 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className='px-4 p-2'
+        className='px-4 p-2 rounded-full text-sm text-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-600 ring-opacity-50'
       />
     </form>
   );
