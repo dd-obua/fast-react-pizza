0064dd38b98870b5f4ab8aec681ca2e24941c655
Obua Denis Daniel <dd.obua>
2024-05-28 18:52:36 +0300

    Change page title and favicon

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className='flex justify-between bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:p-6'>
+    <header className='flex justify-between items-center bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:p-6'>
       <Link to="/" className='tracking-widest'>Fast React Pizza Co.</Link>
       <SearchOrder />
       <Username />
